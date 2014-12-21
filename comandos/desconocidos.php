a680a48c2b113145a8010ac4a5024debdda18445
Carlos Uliao <culiao@opencores.org>
2014-12-21 13:39:30 -0300

    Resulta que los comandos no eran desconocidos

---
+++ comandos/desconocidos.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * desconocidos.php
+ *  
+ * Origen:
+ * Comandos desconocidos. Descifrarlos esta pendiente aun. 
+ * --------------------------------------------------------
+ */
+$comandos = array(
+);
+
+?>
